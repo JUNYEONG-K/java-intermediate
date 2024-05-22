f36330a8e95d15c4ae30a76b843a8c9f94276dec
JUNYEONG-K <godoly1211@naver.com>
2024-05-22 19:27:06 +0900

    제네릭 메서드

---
+++ src/generic/ex4/MethodMain1.java
@@ -0,0 +1,16 @@
+package generic.ex4;
+
+public class MethodMain1 {
+    public static void main(String[] args) {
+        Integer i = 10;
+
+        Object object = GenericMethod.objMethod(10); // Object 타입으로 반환됨.
+        Integer result1 = (Integer) object; // 캐스팅 필요
+
+        // 타입 인자 (Type Argument) 명시적 전달
+        Integer result2 = GenericMethod.<Integer>genericMethod(i); // 타입 캐스팅 필요 없이 잘 나옴.
+
+        Integer result3 = GenericMethod.<Integer>numberMethod(i); // 타입 제한
+        Double result4 = GenericMethod.<Double>numberMethod(20.0);
+    }
+}
