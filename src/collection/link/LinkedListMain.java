068f8ba6fa7b6d4bc6be6e068760144be3bdf4b3
JUNYEONG-K <godoly1211@naver.com>
2024-05-26 18:32:52 +0900

    실제 LinkedList와 MyLinkedList의 내부 구현 비교하기

---
+++ src/collection/link/LinkedListMain.java
@@ -0,0 +1,10 @@
+package collection.link;
+
+import java.util.LinkedList;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        // 내부 구현을 살펴보자. 내가 구현한 MyLinkedListV3 와 비교해보기!
+        LinkedList<String> stringLinkedList = new LinkedList<>();
+    }
+}
