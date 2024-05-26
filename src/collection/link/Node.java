30475d9c14aa1577c06e35026ddbc7f363a57f43
JUNYEONG-K <godoly1211@naver.com>
2024-05-26 13:06:08 +0900

    연결리스트를 자세히 보기 위해, toString 오버라이드

--- src/collection/link/Node.java
+++ src/collection/link/Node.java
@@ -7,4 +7,28 @@ public class Node {
     public Node(Object item) {
         this.item = item;
     }
+
+//    @Override
+//    public String toString() {
+//        return "Node{" +
+//                "item=" + item +
+//                ", next=" + next +
+//                '}';
+//    }
+
+
+    // A -> B -> C
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        Node x = this;
+        stringBuilder.append("[");
+        while (x != null) {
+            stringBuilder.append(x.item);
+            if (x.next != null) stringBuilder.append(" -> ");
+            x = x.next;
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
 }
