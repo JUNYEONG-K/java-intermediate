0a52742cc94dbfb9edb4f5748a6d09a556f40597
JUNYEONG-K <godoly1211@naver.com>
2024-06-01 12:07:41 +0900

    객체의 원래 참조값 조회

--- src/collection/set/member/HashAndEqualsMain2.java
+++ src/collection/set/member/HashAndEqualsMain2.java
@@ -12,6 +12,9 @@ public class HashAndEqualsMain2 {
         System.out.println("m2.hashCode() = " + m2.hashCode());
         System.out.println("m1.equals(m2) = " + m1.equals(m2));
 
+        System.out.println("System.ref(m1) = " + System.identityHashCode(m1));
+        System.out.println("System.ref(m2) = " + System.identityHashCode(m2));
+
         // 증복 등록됨.
         set.add(m1);
         set.add(m2);
@@ -21,6 +24,7 @@ public class HashAndEqualsMain2 {
         // 검색 실패, 버킷의 같은 인덱스에 저장될 뿐, equals 가 false 이기 때문에 중복 등록 되는 것은 물론 검색도 안된다.
         MemberOnlyHash searchValue = new MemberOnlyHash("First");
         System.out.println("searchValue.hashCode() = " + searchValue.hashCode());
+        System.out.println("System.ref(searchValue) = " + System.identityHashCode(searchValue));
         boolean contains = set.contains(searchValue);
         System.out.println("contains = " + contains);
     }
