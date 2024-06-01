865b587603b58de4b57a7ec3c91b9bdad08018e5
JUNYEONG-K <godoly1211@naver.com>
2024-06-01 12:03:04 +0900

    hashCode 는 재정의 했지만, equals 는 재정의하지 않은 경우

--- src/collection/set/member/HashAndEqualsMain2.java
+++ src/collection/set/member/HashAndEqualsMain2.java
@@ -2,12 +2,12 @@ package collection.set.member;
 
 import collection.set.MyHashSetV2;
 
-public class HashAndEqualsMain1 {
+public class HashAndEqualsMain2 {
 
     public static void main(String[] args) {
         MyHashSetV2 set = new MyHashSetV2(10);
-        MemberNoHashNoEq m1 = new MemberNoHashNoEq("First");
-        MemberNoHashNoEq m2 = new MemberNoHashNoEq("First");
+        MemberOnlyHash m1 = new MemberOnlyHash("First");
+        MemberOnlyHash m2 = new MemberOnlyHash("First");
         System.out.println("m1.hashCode() = " + m1.hashCode());
         System.out.println("m2.hashCode() = " + m2.hashCode());
         System.out.println("m1.equals(m2) = " + m1.equals(m2));
@@ -18,8 +18,8 @@ public class HashAndEqualsMain1 {
 
         System.out.println("set = " + set);
 
-        // 검색 실패
-        MemberNoHashNoEq searchValue = new MemberNoHashNoEq("First");
+        // 검색 실패, 버킷의 같은 인덱스에 저장될 뿐, equals 가 false 이기 때문에 중복 등록 되는 것은 물론 검색도 안된다.
+        MemberOnlyHash searchValue = new MemberOnlyHash("First");
         System.out.println("searchValue.hashCode() = " + searchValue.hashCode());
         boolean contains = set.contains(searchValue);
         System.out.println("contains = " + contains);
